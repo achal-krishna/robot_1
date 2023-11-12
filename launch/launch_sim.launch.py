dc92696369db19bf7c3cbf70057a7f26e5b5b3b7
achal <krishna.achal@gmail.com>
2023-11-12 18:05:01 +0530

    SLAM files

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -18,6 +18,12 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true',"use_ros2_control":"true"}.items()
+    )
+
+    joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','joystick.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
@@ -27,7 +33,7 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    #launch_arguments={'extra_gazebo_args': '--ros-args --params-file'+ gazebo_params_file}.items()
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file'+ gazebo_params_file}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
@@ -49,6 +55,7 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
+        joystick,
         gazebo,
         spawn_entity,
         diff_drive_spawner,
